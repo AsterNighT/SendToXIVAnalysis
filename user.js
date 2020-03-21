920bc51b773f0d4bd1bb0e0abb53e73ded19f7dc
AsterNighT <klxjt99@outlook.com>
2020-03-21 05:12:24 +0000

    init

---
+++ user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Send to xivanalysis
+// @namespace    Not yet
+// @version      0.1
+// @description  Send your fflogs to xivanalysis
+// @author       Meyas
+// @match        https://*.fflogs.com/reports/*
+// @match        https://fflogs.com/reports/*
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+})();
