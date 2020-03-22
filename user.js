2862579f3e7262b1da45419b555e90ed1a8f1c73
AsterNighT <klxjt99@outlook.com>
2020-03-22 08:11:35 +0000

    done

--- user.js
+++ user.js
@@ -6,10 +6,16 @@
 // @author       Meyas
 // @match        https://*.fflogs.com/reports/*
 // @match        https://fflogs.com/reports/*
+// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 (function () {
     'use strict';
-
+    $('#main-menu').append('<input type="button" value="XIVAnalysis" id="sendtoxivanalysis" class="right visible">')
+    $("#sendtoxivanalysis").css("position", "relative").css("top", 10);
+    $('#sendtoxivanalysis').click(function(){
+        const hash = window.location.href.split('/').pop();
+        window.open(`https://xivanalysis.com/find/${hash}`,'_blank');
+    });
     // Your code here...
 })();
